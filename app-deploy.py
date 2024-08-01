4406a7bca365eb00a03fd7526bccb04a2d9c0fdd
T-Bradley <96028987+T-Bradley@users.noreply.github.com>
2024-08-01 14:50:25 +0900

    Update app-deploy.py

--- app-deploy.py
+++ app-deploy.py
@@ -50,11 +50,7 @@ def play_video(video_source):
 
     st.video(temp_file.name)
 
-st.set_page_config(
-    page_title = "AI Fire Safety Project", 
-    page_icon = ":fire:",
-    layout = "centered", 
-    initial_sidebar_state = "expanded")
+ 
 
 hide_github_icon = """
 #GithubIcon {
